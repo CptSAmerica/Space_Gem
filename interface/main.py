77f19a71fad352a8acd48dbfbe432484df7472ed
Shaked Keidar <shakedk23@gmail.com>
2025-03-17 15:46:41 +0000

    Baseline model is built. Starting to improve it.

--- interface/main.py
+++ interface/main.py
@@ -4,3 +4,13 @@ import pandas as pd
 from pathlib import Path
 from colorama import Fore, Style
 from dateutil.parser import parse
+
+
+def retreive_data() -> pd.DataFrame:
+    '''
+    - Look for the data locally.
+    - If exists, create 2 dfs: train and test
+    - If not, query the database on BigQuery, download it locally and create the dfs
+    '''
+
+    print(Fore.MAGENTA + "\n ⭐️ Use case: create dfs" + Style.RESET_ALL)
