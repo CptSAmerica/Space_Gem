7cbb29774fecbc5853832ad6f7f8f713b3efbf3d
Vinodhabiz <vi2rabiz@gmail.com>
2025-03-24 13:27:45 +0100

    modified fast.py and requirements.txt

--- api/fast.py
+++ api/fast.py
@@ -10,21 +10,22 @@ import uvicorn
 app = FastAPI()
 
 model_path = os.path.dirname(os.path.dirname(__file__))
-absolute_path = os.path.join(model_path, "raw_data", "model_v2_1.keras")
+absolute_path = os.path.join(model_path, "raw_data", "model_v2_3.keras")
+#absolute_path = os.path.join(model_path, "raw_data", "model_v2_1.keras")
 
-
-#absolute_path = "/home/vinodha/code/CptSAmerica/Space_Gem/raw_data/model_v2_1.keras"
+#absolute_path = "/home/vinodha/code/CptSAmerica/Space_Gem/raw_data/model_v2_225.keras"
 # Load the model from the .keras file shared by Jerome
-#absolute_path = "model_v2_1.keras"
+#absolute_path = "model_v2_225.keras"
 model = load_model(absolute_path)
 #model.summary()
 
 # Preprocessing function for the uploaded image
 def preprocess_image(image_bytes):
-    img = image.load_img(BytesIO(image_bytes), target_size=(255, 255))
+    #img = image.load_img(BytesIO(image_bytes), target_size=(225, 225))
+    img = image.load_img(BytesIO(image_bytes), target_size=(225, 225))
     img_array = image.img_to_array(img)
-    img_array = img_array / 255.0  # Normalize pixel values
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension (1, 255, 255, 3)
+    #img_array = img_array / 255.0  # Normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension (1, 225, 225, 3)
     return img_array
 
 # Load gemstone classes from the JSON file
@@ -38,6 +39,7 @@ def get_gemstone_label(image_bytes):
     processed_image = preprocess_image(image_bytes)
     print(f"Processed image shape: {processed_image.shape}")
     prediction = model.predict(processed_image)
+    print (prediction)
     predicted_class_index = np.argmax(prediction[0])
     print(f"Predicted class index: {predicted_class_index}")
     # Using gemstone_classes list
